09db01315f5f497ed82dc74892ff1013d18430dc
Firsoova08 <foxalishca@mail.ru>
2025-10-16 17:38:06 +0300

    4

---
+++ FirsovaHW7/Dice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirsovaHW7
+{
+    internal class Dice
+    {
+        private int _sides;
+        private Random _random;
+        public Dice(int sides = 6)
+        {
+            this._sides = sides;
+            this._random = new Random();
+        }
+        public int Roll()
+        {
+            return _random.Next(1, _sides + 1);  
+        }
+        public string GetInfo()
+        {
+            return $"{_sides}-гранный кубик";
+        }
+
+    }
+}
--- FirsovaHW7/Program.cs
+++ FirsovaHW7/Program.cs
@@ -22,16 +22,29 @@
             //account.PrintBalance();
 
             //3
-            Car myCar = new Car("Toyota camry 2.5", 250);
-            myCar.PrintInfo();
-            Console.WriteLine("\n--- Разгон ---");
-            myCar.Accelerate(50);
-            myCar.Accelerate(70);
-            myCar.PrintInfo();
-            Console.WriteLine("\n--- Торможение ---");
-            myCar.Brake();
-            myCar.PrintInfo();
+            //Car myCar = new Car("Toyota camry 2.5", 250);
+            //myCar.PrintInfo();
+            //Console.WriteLine("\n--- Разгон ---");
+            //myCar.Accelerate(50);
+            //myCar.Accelerate(70);
+            //myCar.PrintInfo();
+            //Console.WriteLine("\n--- Торможение ---");
+            //myCar.Brake();
+            //myCar.PrintInfo();
 
+            //4
+            //Dice dice6 = new Dice();           
+            //Dice dice12 = new Dice(12);
+            //Console.Write($"{dice6.GetInfo()}: ");
+            //int roll1 = dice6.Roll();
+            //int roll2 = dice6.Roll();
+            //int roll3 = dice6.Roll();
+            //Console.WriteLine($"{roll1}, {roll2}, {roll3}");
+            //Console.Write($"{dice12.GetInfo()}: ");
+            //int roll4 = dice12.Roll();
+            //int roll5 = dice12.Roll();
+            //int roll6 = dice12.Roll();
+            //Console.WriteLine($"{roll4}, {roll5}, {roll6}");
         }
     }
 }
