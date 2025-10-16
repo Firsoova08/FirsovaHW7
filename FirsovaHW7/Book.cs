f829344744f5ccdbe15458dab02d0b3d0a06f996
Firsoova08 <foxalishca@mail.ru>
2025-10-16 16:57:50 +0300

    Add project files.

---
+++ FirsovaHW7/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirsovaHW7
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+        public bool IsAvailable;
+
+        public Book()
+        {
+            Title = "Неизвестно";
+            Author = "Неизвестен";
+            Year = 0;
+            IsAvailable = false;
+        }
+        public Book(string bookTitle, string bookAuthor, int bookYear, bool bookAvailable)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            Year = bookYear;
+            IsAvailable = bookAvailable;
+        }
+        public void ShowInfo()
+        {
+            string available = IsAvailable ? "Да" : "Нет";
+            Console.WriteLine($"Книга: \"{Title}\"");
+            Console.WriteLine($"Автор: {Author}");
+            Console.WriteLine($"Год: {Year}");
+            Console.WriteLine($"Доступна: {available}");
+        
+        }
+    }
+}
+
---
+++ FirsovaHW7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FirsovaHW7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Book unknownBook = new Book();
+            //unknownBook.ShowInfo();
+            //Book warAndPeace = new Book("Война и мир", "Толстой", 1869, true);
+            //warAndPeace.ShowInfo();
+            //Book masterAndMargarita = new Book("Мастер и Маргарита", "Булгаков", 1966, false);
+            //masterAndMargarita.ShowInfo();
+
+
+        }
+    }
+}
